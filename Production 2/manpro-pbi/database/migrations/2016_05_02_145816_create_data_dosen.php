cfb10a4105820fb9166e87767429a08450fcbbae
Yudha Kurniawan <yudhaskurniawan@gmail.com>
2016-05-02 22:11:49 +0700

    Update migration data Dosen , Model data Dosen

---
+++ Production 2/manpro-pbi/database/migrations/2016_05_02_145816_create_data_dosen.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataDosen extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('data_dosen',function(Blueprint $table)
+      {
+        $table->increments('id');
+        $table->string('nama');
+        $table->string('jabatan');
+        $table->text('profile');
+        $table->string('foto');
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('data_dosen');
+    }
+}

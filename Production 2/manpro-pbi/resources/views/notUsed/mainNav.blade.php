9c7daa16d2752bd94d94a502727913f932f5b295
Michael Buntoro <michael.kristianto@ti.ukdw.ac.id>
2016-05-02 11:04:24 +0700

    view yang ga kepakai dipindah, dijadikan satu folder

---
+++ Production 2/manpro-pbi/resources/views/notUsed/mainNav.blade.php
@@ -0,0 +1,48 @@
+
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top absolute-wrapper">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- <a class="navbar-brand page-scroll" href="#page-top"><img src="img/logo-01.png" width="250px" style="margin-top: -50px"></a> -->
+            <a class="navbar-brand page-scroll" href="#page-top"><img src="img/header.png" width="30px" id="logo_header">Pendidikan Bahasa Inggris</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li>
+                    <a class="page-scroll" href="#page-top">Home</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#visi-misi">Visi Misi</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#lect-profile">Profil</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#program_pelatihan">Program</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#portfolio">Publikasi</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#contact">Hubungi Kami</a>
+                </li>
+                <li>
+                  <a class="navbar-brand page-scroll" href="#page-top"><img src="img/Flag_of_Indonesia.svg.png" width="30px" id="logo_bendera"></a>
+                </li>
+                <li>
+                    <a class="navbar-brand page-scroll" href="#page-top"><img src="img/uk-flag.jpg" width="30px" height="20px" id="logo_bendera"></a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>

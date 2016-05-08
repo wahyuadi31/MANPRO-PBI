c72c40272f215151c887d12b4064fce422b5cbdf
RuddyCahyanto <ruddy.c@ti.ukdw.ac.id>
2016-05-08 14:30:40 +0700

    no message

--- Production 2/manpro-pbi/resources/views/Static/home.blade.php
+++ Production 2/manpro-pbi/resources/views/Static/home.blade.php
@@ -211,11 +211,11 @@
                 <div class="col-lg-8 col-lg-offset-2 text-center">
                     <h2 class="section-heading">{!! Lang::get('home.programtraining') !!}</h2>
                     <hr class="primary">
-                    <div id="wrapper" style="height: 328px; width: 960px">
+                    <div id="wrapper" style="height: 328px; width: 960px;">
                         <div class="slider-wrapper theme-default">
                             <div id="slidder" class="nivoSlider">
                                 <a href="">
-<<<<<<< HEAD:Production 2/manpro-pbi/resources/views/Static/home.blade.php
+
                                   <img src="/img/IECC.jpg" data-thumb="img/IECC.jpg" alt="" title="Intensive English Conversation Class TOEFL"/>
                                 </a>
                                 <a href="">
@@ -226,7 +226,7 @@
                                 </a>
                                 <a href="English Corner">
                                   <img src="/img/English_Corner.jpg" data-thumb="img/English_Corner.jpg" alt="" title="English Corner Program">
-=======
+
                                     <img src="/img/IECC.jpg" alt="" title="Intensive English Conversation Class TOEFL"/>
                                 </a>
                                 <a href="">
@@ -237,7 +237,7 @@
                                 </a>
                                 <a href="English Corner">
                                     <img src="/img/English_Corner.jpg" alt="" title="English Corner Program">
->>>>>>> 31f0dfe25df84cbc1d49c6aa74a27886f62551c4:Production 2/manpro-pbi/resources/views/static/home.blade.php
+
                                 </a>
                             </div>
                             <div id="htmlcaption" class="nivo-html-caption">

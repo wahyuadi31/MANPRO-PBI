b3c7de87376b2bf160b1e35cba2529a8baf3bc4c
cindi2695 <dianpermata@ti.ukdw.ac.id>
2016-05-08 14:40:28 +0700

    data thumbnya sama ukurannya jangan dibalikin lagi dong :')

--- Production 2/manpro-pbi/resources/views/Static/home.blade.php
+++ Production 2/manpro-pbi/resources/views/Static/home.blade.php
@@ -213,21 +213,21 @@
                     <hr class="primary">
 <<<<<<< HEAD
 <<<<<<< HEAD
-                    <div id="wrapper" style="height: 328px; width: 960px;">
+                    <div id="wrapper" style="height: 50%; width: 130%;">
                         <div class="slider-wrapper theme-default">
                             <div id="slidder" class="nivoSlider">
                                 <a href="">
 
-                                  <img src="/img/IECC.jpg" data-thumb="img/IECC.jpg" alt="" title="Intensive English Conversation Class TOEFL"/>
+                                  <img src="/img/IECC.jpg" alt="" title="Intensive English Conversation Class TOEFL"/>
                                 </a>
                                 <a href="">
-                                  <img src="/img/Culture_Event.jpg" data-thumb="img/Culture_Event.jpg" alt="" title="Culture_Event">
+                                  <img src="/img/Culture_Event.jpg" alt="" title="Culture_Event">
                                 </a>
                                 <a href=""
-                                  <img src="/img/ICE.jpg" data-thumb="img/ICE.jpg" alt="" title="Introduction to College English"/>
+                                  <img src="/img/ICE.jpg" alt="" title="Introduction to College English"/>
                                 </a>
                                 <a href="English Corner">
-                                  <img src="/img/English_Corner.jpg" data-thumb="img/English_Corner.jpg" alt="" title="English Corner Program">
+                                  <img src="/img/English_Corner.jpg" alt="" title="English Corner Program">
 
 =======
                     <div id="wrapper" style="height: 50%; width: 100%">

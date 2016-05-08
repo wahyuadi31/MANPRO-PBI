31f0dfe25df84cbc1d49c6aa74a27886f62551c4
cindi2695 <dianpermata@ti.ukdw.ac.id>
2016-05-08 14:06:32 +0700

    coba hilangkan data thumb

--- Production 2/manpro-pbi/resources/views/static/home.blade.php
+++ Production 2/manpro-pbi/resources/views/static/home.blade.php
@@ -211,29 +211,21 @@
                 <div class="col-lg-8 col-lg-offset-2 text-center">
                     <h2 class="section-heading">{!! Lang::get('home.programtraining') !!}</h2>
                     <hr class="primary">
-                    <div id="wrapper">
+                    <div id="wrapper" style="height: 328px; width: 960px">
                         <div class="slider-wrapper theme-default">
                             <div id="slidder" class="nivoSlider">
-                                <div style="height: 328px; width: 960px">
-                                    <a href="">
-                                        <img src="/img/IECC.jpg" data-thumb="img/IECC.jpg" alt="" title="Intensive English Conversation Class TOEFL"/>
-                                    </a>
-                                </div>
-                                <div style="height: 328px; width: 960px">
-                                    <a href="">
-                                        <img src="/img/Culture_Event.jpg" data-thumb="img/Culture_Event.jpg" alt="" title="Culture_Event">
-                                    </a>
-                                </div>
-                                <div style="height: 328px; width: 960px">
-                                    <a href="">
-                                        <img src="/img/ICE.jpg" data-thumb="img/ICE.jpg" alt="" title="Introduction to College English"/>
-                                    </a>
-                                </div>
-                                <div style="height: 328px; width: 960px">
-                                    <a href="English Corner">
-                                        <img src="/img/English_Corner.jpg" data-thumb="img/English_Corner.jpg" alt="" title="English Corner Program">
-                                    </a>
-                                </div>
+                                <a href="">
+                                    <img src="/img/IECC.jpg" alt="" title="Intensive English Conversation Class TOEFL"/>
+                                </a>
+                                <a href="">
+                                    <img src="/img/Culture_Event.jpg" alt="" title="Culture Event">
+                                </a>
+                                <a href="">
+                                    <img src="/img/ICE.jpg" alt="" title="Introduction to College English"/>
+                                </a>
+                                <a href="English Corner">
+                                    <img src="/img/English_Corner.jpg" alt="" title="English Corner Program">
+                                </a>
                             </div>
                             <div id="htmlcaption" class="nivo-html-caption">
                             </div>
